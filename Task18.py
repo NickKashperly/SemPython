865ca59e640c5f297b1067f8898b97fe97d572f2
Nick <nikkashin659@gmai.com>
2023-03-23 12:00:13 +0300

    Homework3 r your changes. Lines starting

--- Task18.py
+++ Task18.py
@@ -3,18 +3,18 @@ a = []
 n = int(input("Введите количество числе в списке:  "))
 x = int(input("Искомое число:  "))
 for i in range(n):
-    a.append(randint(1,10))
+    a.append(randint(1, 100))
 print(a)
 j = 0
 find_res = 0
 flag = True
 while find_res == 0:
     i = 0
-    while i<n and flag:
-        if abs(x-a[i])==j:
+    while i < n and flag:
+        if abs(x-a[i]) == j:
             find_res = a[i]
             flag = False
-        i+=1
-    j+=1
+        i += 1
+    j += 1
 
 print(f'res = {find_res}')
